b736bd391a02ee0bda12a9e9aea896baab6a22ec
Poux- - Bories Yoan <yoan.poux-bories@etu.iut-tlse3.fr>
2024-11-04 16:05:12 +0100

    suppression

--- DiagrammeSAE3/src/modele/TypeAssurance.java
+++
@@ -1,8 +0,0 @@
-package modele;
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
-
-@objid ("97df7b1b-ae09-41b0-814a-ac23146a649c")
-public enum TypeAssurance {
-    PROPRIETAIRE,
-    AIDE_JURIDIQUE;
-}
