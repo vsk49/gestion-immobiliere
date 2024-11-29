a5f2fbb6c0eb7fe5e9c509643e1a7e8386695c68
Poux- - Bories Yoan <yoan.poux-bories@etu.iut-tlse3.fr>
2024-11-29 17:03:59 +0100

    WorkInProgress

--- src/vue/IHMDetailsLocataire.java
+++ src/vue/IHMDetailsLocataire.java
@@ -6,6 +6,8 @@ import java.awt.GridLayout;
 import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
